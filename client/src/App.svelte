62c53e812ea0dc380c089e0f5ff60be34697d47a
Gustav <gullegustav@gmail.com>
2022-04-12 17:17:37 +0200

    Work completed on frontend mainly css and cleanup as well as added photos and fixed the search bar. the navigation-bar is not at a good looking shipable state.

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -11,26 +11,42 @@
 
 	//variables
 	let inputSearch = "";
-	
-	//functions
-	function handleSearch() {
-		searchStore.set(inputSearch);
-		console.log("Word", inputSearch);
 
-		document.getElementById("search-link").click();
+	//Images src
+	let srcKeaBoutique = "/images/KeaBoutique.png";
+	let srcCategories = "/images/Categories.png";
+	let srcCart = "/images/Cart.png";
+
+	//functions
+	function handleInputEnter(event) {
+		if (event.key === "Enter") {
+			if (inputSearch !== "") {
+				searchStore.set(inputSearch);
+				document.getElementById("search-link").click();
+			};
+		};
 	};
+	function handleClick() {
+		if (inputSearch !== "") {
+			searchStore.set(inputSearch);
+			document.getElementById("search-link").click();
+		};
+	};
+
+
 </script>
 
 <Router>
     <nav>
-        <Link to="/">Home</Link>
-		<input bind:value={inputSearch} placeholder="search for a product" on:change={handleSearch}/>
-		<Link id="search-link" to="/search?s={inputSearch}"></Link>
-        <Link to="/categories">Categories</Link>
-        <Link to="/cart">🛒</Link>
-
+        <Link to="/"><img src={srcKeaBoutique} alt="Kea Boutique"/></Link>
+		<div id="search-area">
+			<input type="search" bind:value={inputSearch} placeholder="search for a product" on:keyup={handleInputEnter}/>
+			<button on:click={handleClick} type="submit">Search</button>
+			<Link id="search-link" to="/search?s={inputSearch}"></Link>
+		</div>
+        <Link to="/categories"><img src={srcCategories} alt="Categories"/></Link>
+        <Link to="/cart"><img src={srcCart} alt="Cart"/><b id="cart-count">(1)</b></Link>
     </nav>
-    
 	<Route path="/" component={Home} />
     <Route path="/cart" component={Cart} />
     <Route path="/categories" component={Categories} />
@@ -38,3 +54,27 @@
 
 </Router>
 
+<style>
+	nav {
+		display: flex;
+		background-color: #c5c5c5;
+		justify-content: space-between;
+		align-items: center;
+		padding: 0.5em;
+		font-size: 1.5em;
+		text-decoration: none;
+		color: black;
+	}
+	#search-area {
+		padding-top: 0.3em;
+	}
+	#search-area input {
+		width: 20em;
+	}
+	#cart-count {
+		position: relative;
+		bottom: 20px;
+		color: #7a0000
+	}
+
+</style>
